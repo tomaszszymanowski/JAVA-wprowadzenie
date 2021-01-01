9544067d78536aa35c0d89eafaa52ac81f8538aa
tomaszszymanowski <tomasz.szymanowski@gmail.com>
2021-01-01 19:18:03 +0100

    #5 loop for - task 3

---
+++ src/LoopFor/Task3.java
@@ -0,0 +1,13 @@
+package LoopFor;
+
+public class Task3 {
+    public static void main(String[] args) {
+        char letter = 98;
+
+        for (int i = 97; i < 123; i++) {
+            letter = (char) i;
+            System.out.print(letter + ", ");
+
+        }
+    }
+}
